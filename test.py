10e7e19a5bc0f4592718668f5a5197a8e6b63518
foggystar <fdgaojx@126.com>
2025-01-14 17:34:47 +0800

    tests

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
 import dspy
+print("1")
 import os
 API_Key = os.getenv('DEEPSEEK_API_KEY')
-lm = dspy.LM('deepseek-chat', api_base='https://api.deepseek.com', api_key=API_Key)
+# deepseek-ai/DeepSeek-V3
+lm = dspy.LM('deepseek-chat', api_base='https://api.deepseek.com/v1', api_key=API_Key)
 dspy.configure(lm=lm)
+try:
+    print(lm("Say this is a test!", temperature=0.7))  # => ['This is a test!']
+    print("Success")
+except Exception as e:
+    print(e)
+# lm(messages=[{"role": "user", "content": "Say this is a test!"}])  # => ['This is a test!']
