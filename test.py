fc96b0180b227d44485529d5bf193b3784e618cb
foggystar <fdgaojx@126.com>
2025-01-14 19:01:27 +0800

    tests

--- test.py
+++ test.py
@@ -1,13 +1,10 @@
-import dspy
-print("1")
+import dspy # It takes too long to import the package
 import os
 API_Key = os.getenv('DEEPSEEK_API_KEY')
 # deepseek-ai/DeepSeek-V3
 lm = dspy.LM('deepseek-chat', api_base='https://api.deepseek.com/v1', api_key=API_Key)
 dspy.configure(lm=lm)
-try:
-    print(lm("Say this is a test!", temperature=0.7))  # => ['This is a test!']
-    print("Success")
-except Exception as e:
-    print(e)
-# lm(messages=[{"role": "user", "content": "Say this is a test!"}])  # => ['This is a test!']
+print(lm("Say this is a test!", temperature=0.7))  # => ['This is a test!']
+
+math = dspy.ChainOfThought("question -> answer: float")
+math(question="Two dice are tossed. What is the probability that the sum equals two?")
