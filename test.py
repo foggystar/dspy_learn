6b717bbd46e9b278e226b11e52da0034b89ae3b9
foggystar <fdgaojx@126.com>
2025-01-15 16:34:49 +0800

    tests

--- test.py
+++ test.py
@@ -8,3 +8,21 @@ print(lm("Say this is a test!", temperature=0.7))  # => ['This is a test!']
 
 math = dspy.ChainOfThought("question -> answer: float")
 math(question="Two dice are tossed. What is the probability that the sum equals two?")
+
+class ExtractInfo(dspy.Signature):
+    """Extract structured information from text."""
+
+    text: str = dspy.InputField()
+    title: str = dspy.OutputField()
+    headings: list[str] = dspy.OutputField()
+    entities: list[dict[str, str]] = dspy.OutputField(desc="a list of entities and their metadata")
+
+module = dspy.Predict(ExtractInfo)
+
+text = "Apple Inc. announced its latest iPhone 14 today." \
+    "The CEO, Tim Cook, highlighted its new features in a press release."
+response = module(text=text)
+
+print(response.title)
+print(response.headings)
+print(response.entities)
